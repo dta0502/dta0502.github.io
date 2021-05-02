b570c817e9f1584d3118e1e535884ef68553da36
Richard <dta0502@126.com>
2021-05-02 20:53:01 +0800

    Revert "[ImgBot] Optimize images"

--- custom/fold_action.js
+++
@@ -1,3 +0,0 @@
-// build time:Mon Dec 23 2019 23:29:55 GMT+0800 (中国标准时间)
-$(document).ready(function(){$(document).on("click",".fold_hider",function(){$(">.fold",this.parentNode).slideToggle();$(">:first",this).toggleClass("open")});$("div.fold").css("display","none")});
-//rebuild by neat 
