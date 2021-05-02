d3ed16e00a424890ad3b5eda693a4d89c86fe686
Richard <dta0502@126.com>
2021-05-02 20:53:01 +0800

    Revert "[ImgBot] Optimize images"

--- js/scroll-cookie.js
+++
@@ -1,3 +0,0 @@
-// build time:Mon Dec 23 2019 23:29:55 GMT+0800 (中国标准时间)
-$(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]===o){$(window).scrollTop(e[0])}}});
-//rebuild by neat 
